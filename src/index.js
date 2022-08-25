94786804af15628b23c69412804d3a6c6a7d5203
Muriithi Ian <imuriithiian@gmail.com>
2022-08-25 14:35:00 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+	// your code here
+	const todo = document.getElementById("tasks");
+	const form = document.getElementById("create-task-form");
+	const task = document.getElementById("new-task-description");
+	const addedTasks = [];
+
+	form.addEventListener("submit", (e) => {
+		e.preventDefault();
+		task.value.length > 0 ? addedTasks.push(task.value) : null;
+		addListItem();
+		form.reset();
+	});
+
+	function addListItem() {
+		todo.replaceChildren();
+
+		addedTasks.forEach((task, index) => {
+			let li = document.createElement("li");
+			li.setAttribute("id", index);
+			li.textContent = task;
+			todo.appendChild(li);
+		});
+	}
 });
+
